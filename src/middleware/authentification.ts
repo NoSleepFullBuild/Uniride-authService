9d1ecd2a17e7cfc9552baabc3495870c5d7ee198
MTthoas <matthias.pecquery@gmail.com>
2024-03-03 21:22:01 +0100

    (feat) setup npmrc.js

--- src/middleware/authentification.ts
+++ src/middleware/authentification.ts
@@ -1,25 +1,24 @@
 export class Authentification {
+  public checkoutEmail = (email: string): boolean => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  };
 
-    public checkoutEmail = (email: string): boolean => {
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        return emailRegex.test(email);
-    }
+  public checkoutUsername = (username: string): boolean => {
+    const usernameRegex = /^[a-zA-Z0-9._-]{3,}$/;
+    return usernameRegex.test(username);
+  };
 
-    public checkoutUsername = (username: string): boolean => {
-        const usernameRegex = /^[a-zA-Z0-9._-]{3,}$/;
-        return usernameRegex.test(username);
-    }
+  public checkoutPassword = (password: string): boolean => {
+    // Just verify if there is 8 characters
+    const passwordRegex = /^.{8,}$/;
+    return passwordRegex.test(password);
+  };
 
-    public checkoutPassword = (password: string): boolean => {
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-        return passwordRegex.test(password);
+  public checkoutToken = (token: string): boolean => {
+    if (token && token == "briatte") {
+      return true;
     }
-
-    public checkoutToken = (token: string): boolean => {
-        if(token && token == "briatte"){
-            return true
-        }
-        return false
-    }
-    
+    return false;
+  };
 }
