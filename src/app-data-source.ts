8593641fd6d02d42312837913d41f032d0997706
MTthoas <matthias.pecquery@gmail.com>
2024-03-04 00:01:46 +0100

    (feat) update app-data-source and docker-compose

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -3,14 +3,17 @@ import 'reflect-metadata';
 import { Auth } from "@nosleepfullbuild/uniride-library/dist/entity/auth/auth.entity";
 import { Token } from "@nosleepfullbuild/uniride-library/dist/entity/token/token.entity";
 
+// read .env
+require('dotenv').config();
+
 export const AppDataSource = new DataSource({
     host: 'localhost',
-    port: 5433,
-    username: 'user',
-    password: 'password',
-    database: 'postgresAuth',
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     type: 'postgres',
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: [Auth, Token],
     // migrations: ['src/migrations/**/*{.ts,.js}'],
