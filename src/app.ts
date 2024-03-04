52284347d0bd8bc7007737f5505b7408db112c31
MTthoas <matthias.pecquery@gmail.com>
2024-03-04 23:45:11 +0100

    (feat) correction of brain less

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,6 @@ app.get('/api/auth/verify-token', authController.verifyToken.bind(authController
 app.post('/api/auth/logout', checkJwt, authController.logout.bind(authController));
 app.get('/api/auth/whoiam', authController.whoIam.bind(authController));
 
-app.listen(3005, () => {
-    console.log("Auth service is running on port 3005");
+app.listen(3002, () => {
+    console.log("Auth service is running on port 3002");
 });
